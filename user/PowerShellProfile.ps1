122a9d31843f6eb85f57b4f220df51eef2f5c550
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2021-01-16 16:43:00 -0500

    Removed Transcript and Alias

--- user/PowerShellProfile.ps1
+++ user/PowerShellProfile.ps1
@@ -1,4 +1,2 @@
-function log { Start-Transcript -OutputDirectory "$HOME\source\repos\ididevsecops\Repo1\transcript" }
-Write-Host "Type 'log' to start a transcript."
 function repo { Set-Location -Path "$HOME\source\repos\ididevsecops\Repo1" }
 Write-Host "Type 'repo' to switch to Repo1."
