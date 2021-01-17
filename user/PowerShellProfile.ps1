84634f33006eedd753a8f51337eff52657b941fd
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2021-01-16 21:21:44 -0500

    Added PowerShell Profile Global Variables for Blueprint Assignments

--- user/PowerShellProfile.ps1
+++ user/PowerShellProfile.ps1
@@ -7,6 +7,7 @@ Write-Host "Type 'root' to set Azure context to root."
 function root {
     New-Variable -Name "mg" -Scope "Global" -Value $t -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "bp" -Scope "Global" -Value "bp-idso" -Force -Verbose -ErrorAction SilentlyContinue
+    New-Variable -Name "bpa" -Scope "Global" -Value "bpa-idso" -Force -Verbose -ErrorAction SilentlyContinue
     Remove-Variable -Name "sub" -Scope "Global" -Force -Verbose -ErrorAction SilentlyContinue
     Remove-Variable -Name "rg" -Scope "Global" -Force -Verbose -ErrorAction SilentlyContinue
 }
@@ -15,6 +16,7 @@ Write-Host "Type 'corp' to set Azure context to corp."
 function corp {
     New-Variable -Name "mg" -Scope "Global" -Value "mg-corp-idso" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "bp" -Scope "Global" -Value "bp-corp-idso" -Force -Verbose -ErrorAction SilentlyContinue
+    New-Variable -Name "bpa" -Scope "Global" -Value "bpa-corp-idso" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "sub" -Scope "Global" -Value "sub-corp-idso-000" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "rg" -Scope "Global" -Value "rg-shared-corp-idso-000" -Force -Verbose -ErrorAction SilentlyContinue
 }
@@ -23,14 +25,16 @@ Write-Host "Type 'dev' to set Azure context to dev."
 function dev {
     New-Variable -Name "mg" -Scope "Global" -Value "mg-dev-idso" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "bp" -Scope "Global" -Value "bp-dev-idso" -Force -Verbose -ErrorAction SilentlyContinue
+    New-Variable -Name "bpa" -Scope "Global" -Value "bpa-dev-idso" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "sub" -Scope "Global" -Value "sub-dev-idso-000" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "rg" -Scope "Global" -Value "rg-shared-dev-idso-000" -Force -Verbose -ErrorAction SilentlyContinue
 }
 
 Write-Host "Type 'prod' to set Azure context to prod."
 function prod {
-    New-Variable -Name "bp" -Scope "Global" -Value "bp-prod-idso" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "mg" -Scope "Global" -Value "mg-prod-idso" -Force -Verbose -ErrorAction SilentlyContinue
+    New-Variable -Name "bp" -Scope "Global" -Value "bp-prod-idso" -Force -Verbose -ErrorAction SilentlyContinue
+    New-Variable -Name "bpa" -Scope "Global" -Value "bpa-prod-idso" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "sub" -Scope "Global" -Value "sub-prod-idso-000" -Force -Verbose -ErrorAction SilentlyContinue
     New-Variable -Name "rg" -Scope "Global" -Value "rg-shared-prod-idso-000" -Force -Verbose -ErrorAction SilentlyContinue
 }
